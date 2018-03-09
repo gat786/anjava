1f25bc7635b4001afb12a995bb2dc7928060582a
gat786 <ganesht049@gmail.com>
2018-03-09 10:12:08 +0530

    Add files via upload

--- constDemo.java
+++ constDemo.java
@@ -1,23 +1,38 @@
+class divi{
+int a,b;
+
+	divi()					//default constructor
+	{
+		a=20;
+		b=10;
+	}
+	int division()
+	{
+		return b/a;
+	}
+}
+
+
 class add
 {
 
-int a,b;
+	int a,b;  				//global variable
 
-add(int a, int b)
-{
-	this.a=a;
-	this.b=b;
-}
+	add(int a, int b)			//parameterized constructor
+	{
+		this.a=a;
+		this.b=b;
+	}
 
-int addition()
-{
-	return a+b;
-}
+	int addition()
+	{
+		return a+b;
+	}
 
 }
 class subtract
 {
-	int subtraction(int a , int b)
+	int subtraction(int a , int b)  			//local variable
 	{
 		return a-b;
 	}
@@ -27,6 +42,9 @@ public class constDemo
 {
 	public static void main(String args[])
 	{
+		divi d=new divi();
+		System.out.println(d.division());
+
 		add a=new add(10,12);
 		System.out.println(a.addition());
 
